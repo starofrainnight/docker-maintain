f83cd5bbebb79991eb00585f8e020eef20772622
Hong-She Liang <starofrainnight@gmail.com>
2018-01-22 09:57:30 +0800

    Supported bumpversion
    So we now manage versions by bumpversion automatic

--- setup.py
+++ setup.py
@@ -24,11 +24,9 @@ long_description = (
     open("CHANGES.rst", "r").read()
 )
 
-exec(pydgutils.version.read_version_file(source_dir, package_name))
-
 setup(
     name=package_name,
-    version=__version__,
+    version='0.0.1',
     author="Hong-She Liang",
     author_email="starofrainnight@gmail.com",
     url="https://github.com/starofrainnight/%s" % package_name,
@@ -39,8 +37,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Libraries",
     ],
